55a232cf2fa137f078a0cf2030d307bad6074a28
ssjrnath <ssjrnath1@gmail.com>
2019-02-21 22:22:13 +0530

    Removed unused definitions

--- src/twenty48/core.clj
+++ src/twenty48/core.clj
@@ -1,22 +1,6 @@
 (ns twenty48.core
   (:gen-class))
 
-(defn move-grid-right
-  "Moves an entire grid to the right"
-  [grid]
-  grid)
-
-(defn move-grid-down
-  "Moves an entire grid down"
-  [grid]
-  grid)
-
-(defn move-grid-up
-  "Moves an entire grid up"
-  [grid]
-  grid)
-
-
 (def split-identical (comp (partial partition-by identity) (partial remove zero?)))
 
 (def split-by-2 (comp (partial mapcat (partial partition-all 2)) split-identical))
